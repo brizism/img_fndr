3fb5b760f12deda45f44a3b9d379f9e066cedaa9
brizism <montielbriseida@gmail.com>
2018-04-23 19:16:06 -0400

    add Dialog Material component

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -8,6 +8,19 @@ import FlatButton from 'material-ui/FlatButton';
 import { spawn } from 'child_process';
 
 class ImageResults extends Component {
+  state = {
+    open: false,
+    currentImg: ''
+  }
+
+  handleOpen = img => {
+    this.setState({ open: true, currentImg: img })
+  }
+
+  handleClose = () => {
+    this.setState({ open: false })
+  }
+
   render() {
     let imageListContent;
     const { images } = this.props;
@@ -25,7 +38,7 @@ class ImageResults extends Component {
                 </span>
               }
               actionIcon={
-                <IconButton>
+                <IconButton onClick={() => this.handleOpen(img.largeImageURL)}>
                   <ZoomIn color="white" />
                 </IconButton>
               }
@@ -38,9 +51,22 @@ class ImageResults extends Component {
     } else {
       imageListContent = null;
     }
+
+    const actions = [
+      <FlatButton label="Close" primary={true} onClick={this.handleClose} />
+    ]
+
     return (
       <div>
         {imageListContent}
+        <Dialog 
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <img src={this.state.currentImg} alt="" style={{ width: '100%'}} />
+        </Dialog>
       </div>
     );
   }
